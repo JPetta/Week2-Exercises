4065d2455a899b9da64604e755985033f2ef400c
JPetta <jovipetraratulangi@gmail.com>
2019-09-29 21:24:34 +0700

    konversi menit

---
+++ konversi-menit.js
@@ -0,0 +1,18 @@
+function konversiMenit(menit) {
+  var jam = Math.floor(menit/60);
+  var menitMod = menit % 60;
+  if (menitMod < 9){
+    console.log(jam+":0"+menitMod);
+  }
+  else if (menitMod>9) {
+    console.log(jam+":"+menitMod);
+  }
+  
+}
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
